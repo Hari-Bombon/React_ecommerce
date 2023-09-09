ce17ecbdffb01d086c94a8acd981a896b9f44b9e
Hari-Bombon <haripriya14022003@gmail.com>
2023-09-09 20:37:19 +0530

    Added the phone and mail link

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import {BiPhoneCall} from  'react-icons/bi'
+import { AiOutlineMail } from 'react-icons/ai';
+import { BiPhoneCall } from 'react-icons/bi';
+import {Link} from 'react-router-dom'
 
 const header = () => {
   return (
@@ -10,12 +12,13 @@ const header = () => {
             <div className='col-6'>
               <p>The Trending outfits at 100% off</p> {/* Fixed the typo in "The" */}
             </div>
-            <div className='col-6' d-flex-justify-content-between>
+            <div className='col-6 d-flex-justify-content-between'>
               <div>
-              <a href='tel:+9743189032'> <BiPhoneCall /> Call us +7543189032</a>
+              <a href='tel:+9743189032'>  Call us +7543189032</a>
               </div>
               <div>
-
+            <Link>  <BiPhoneCall className='fs-3 mx-4' /></Link>
+            <Link>  <AiOutlineMail className='fs-3 mx-4'/> </Link>
               </div>
             </div>
           </div>
