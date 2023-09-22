34ece6926b7c707892c27baff98d3b249ff612af
Hari-Bombon <haripriya14022003@gmail.com>
2023-09-22 20:03:57 +0530

    Completed the footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -16,13 +16,67 @@ const Footer = () => {
             <p className='mb-3'><b>Phone:</b> <a href='tel:+9743189032'>  Call us +7543189032</a></p>
             <p className='mb-5'><b>Hours Open:</b> From 10 A.M to 11 P.M</p>
              <p className='mb-3'><b>Follow Us</b></p>
-              <div className="col-3 social-icons d-flex justify-content-around">
-                <Link><AiFillTwitterCircle/></Link>
-                <Link><BsFacebook /></Link>
-                <Link><BsInstagram /></Link>
-                <Link><BsLinkedin /></Link>
+              <div className="col-4 social-icons d-flex justify-content-around">
+                <Link><AiFillTwitterCircle className='fs-4'/></Link>
+                <Link><BsFacebook className='fs-4'/></Link>
+                <Link><BsInstagram className='fs-4' /></Link>
+                <Link><BsLinkedin className='fs-4'/></Link>
             </div>
           </div>
+        </div>
+        <div className="col-2">
+          <h3 className='mb-5'>About</h3>
+          <div className="footer-details">
+            <Link className='mb-3 d-flex flex-column'>
+              About Us
+              </Link>
+              <Link className='mb-3'>
+              Delivery
+              </Link>
+              <Link className='mb-3'>
+              Privacy Policy
+              </Link>
+              <Link className='mb-3'>
+              Fee Policy
+              </Link>
+              <Link className='mb-3'>
+              Terms & Conditions
+              </Link>
+          </div>
+        </div>
+        <div className="col-2">
+          <h3 className='mb-5'>Account</h3>
+          <div className="footer-details">
+            <Link className='mb-3 d-flex flex-column'>
+              Profile
+              </Link>
+              <Link className='mb-3'>
+              View Cart
+              </Link>
+              <Link className='mb-3'>
+              My Orders
+              </Link>
+              <Link className='mb-3'>
+              My Wishlist
+              </Link>
+              <Link className='mb-3'>
+              Help
+              </Link>
+              <Link className='mb-3'>
+              Coupons
+              </Link>
+          </div>
+        </div>
+      </div>
+      <hr />
+      <div className="row d-flex justify-content-between">
+        <div className="col-8">
+          <p>&copy; Developed by Haripriya 2023</p>
+        </div>
+        <div className="col-3 d-flex justify-content-between">
+          <Link className='text-black'>Privacy Policy</Link>
+          <Link className='text-black'>Terms of User</Link>
+          <Link className='text-black'>Contact Me</Link>
         </div>
       </div>
     </div>
