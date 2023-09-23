5557ba5d088bb07037bf850a982662ea5099a544
Hari-Bombon <haripriya14022003@gmail.com>
2023-09-23 19:16:48 +0530

    Added the router functionality to the pages

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,13 +3,14 @@ import { AiOutlineMail } from 'react-icons/ai';
 import { BiPhoneCall } from 'react-icons/bi';
 import { AiOutlineMail } from 'react-icons/ai';
 import { VscAccount } from 'react-icons/vsc';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import logo from '../assets/logo.png';
 import { HiOutlineInboxIn } from 'react-icons/hi';
 import { VscAccount } from 'react-icons/vsc';
 import { CgShoppingCart } from 'react-icons/cg';
 
 const Header = () => {
+  const location = useLocation
   return (
     <>
       <header className='header-top-strip p-1 px-4 shadow-sm'>
@@ -44,21 +45,33 @@ const Header = () => {
           </div>
             </div>
             <div className="col-4 d-flex align-items-center justify-content-between text-center">
-              <Link to={'/'}>Home</Link>
-              <Link to={'shop'}>Shop</Link>
-              <Link to={'blogs'}>Blog</Link>
-              <Link to={'about'}>About</Link>
-              <Link to={'contact'}>Contact</Link>
+              <Link to={'/'} className={location.pathname === '/' ? 'active' : inactive}>Home</Link>
+              <Link to={'shop'} className={location.pathname === '/shop' ? 'active' : inactive}>Shop</Link>
+              <Link to={'blogs'} className={location.pathname === '/blog' ? 'active' : inactive}>Blog</Link>
+              <Link to={'about'} className={location.pathname === '/about' ? 'active' : inactive}>About</Link>
+              <Link to={'contact'} className={location.pathname === '/contact' ? 'active' : inactive}>Contact</Link>
             </div>
             <div className="nav-links col-3 d-flex align-item-center justify-content-around">
-              <Link to={'wishlist'}className='d-flex' ><HiOutlineInboxIn className="fs-3 mx-2"/>
-              <p className=''>Wishlist</p>
+              <Link to={'wishlist'}className={location.pathname === '/wishlist' ? 'inactive' : 'active '} >
+                <div className="d-flex">
+                  <span><HiOutlineInboxIn className='fs-3 mx-2'/></span>
+                  <p>Wishlist</p>
+                </div>
               </Link>
-              <Link to={'login'}className='d-flex'><VscAccount className="fs-3 mx-2 "/>
+              <Link to={'login'}className={location.pathname === '/login' ? 'inactive' : 'active '}>
+                <div className="d-flex">
+                  <span>
+                <VscAccount className="fs-3 mx-2 "/>
+                </span>
               <p>Account</p>
+                </div>
               </Link>
-              <Link to={'cart'} className='d-flex'><VscAccount/>
+              <Link to={'cart'} className={location.pathname === '/cart' ? 'inactive' : 'active '}>
+                <div className="d-flex">
+                <span>
               <p><CgShoppingCart className="fs-3 mx-2" />Cart</p>
+                </span>
+                </div>
               </Link>
             </div>
           </div>
