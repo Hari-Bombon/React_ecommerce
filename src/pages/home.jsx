b5ee63dc977240b698b48556b7a2ee4e945c4e5d
Hari-Bombon <haripriya14022003@gmail.com>
2023-09-23 21:33:03 +0530

    Initiated the home page

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 
 const home = () => {
-  return (
-    <div>home</div>
-  )
+  return <>
+  <section className='banner p-5'>
+    <div className="container-xxl">
+      <div className="row">
+      <div className="banner-details">
+        
+      </div>
+      </div>
+    </div>
+  </section>
+  </>
+   
+  
 }
 
 export default home
