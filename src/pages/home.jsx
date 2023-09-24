1b9edb3f72e5ce0fc00cee2dfd5a74862554075a
Hari-Bombon <haripriya14022003@gmail.com>
2023-09-24 21:57:59 +0530

    Added the banner to the home page

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -5,8 +5,9 @@ const home = () => {
   <section className='banner p-5'>
     <div className="container-xxl">
       <div className="row">
-      <div className="banner-details">
-        
+      <div className="banner-details d-flex flex-column align-items-center justify-content-center">
+        <span className='shadow-lg p-4'>Trade in offer !!</span>
+        <h1>Super Value Deals</h1>
       </div>
       </div>
     </div>
