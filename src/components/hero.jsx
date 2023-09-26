9ddeb2049e27ee55d5f6d133deb6f57b3f13f8d7
Hari-Bombon <haripriya14022003@gmail.com>
2023-09-26 22:45:28 +0530

    Added the hero components

---
+++ src/components/hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const hero = () => {
+  return (
+    <div className='hero p-5'>
+        <div className='container-xxl'>
+            <div className='hero-details'></div>
+        </div>
+      </div>
+  )
+}
+
+export default hero
