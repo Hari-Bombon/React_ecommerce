04c1ca2b0f901bea9573e1c38448a2a8b4532dfe
Hari-Bombon <haripriya14022003@gmail.com>
2023-09-27 16:06:15 +0530

    Added the icons

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
+import icon from '../assets/icons/1000_F_408307182_eYKkYshjzXPMSxpjLWIdFOY7lqG0ahF5.jpg'
+
 
 const hero = () => {
   return (
     <div className='hero p-5'>
         <div className='container-xxl'>
-            <div className='hero-details'></div>
+            <div className='hero-details d-flex justify-content-around'>
+              <div className='card p-2'>
+                <img src={icon} alt="" className='img-fluid'/>
+                <p>Fast Orders</p>
+              </div>
+              <div className='card p-2'>
+                <img src={icon} alt="" className='img-fluid'/>
+                <p>Fast Shipping</p>
+              </div>
+              <div className='card p-2'>
+                <img src={icon} alt="" className='img-fluid'/>
+                <p>24*7 Support</p>
+              </div>
+              <div className='card p-2'>
+                <img src={icon} alt="" className='img-fluid'/>
+                <p>High Savings</p>
+              </div>
+            </div>
         </div>
       </div>
   )
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,5 +1,6 @@
 import {React } from 'react'
 import {Link} from 'react-router-dom;'
+import Hero from '../components/hero';
 
 const Home = () => {
   return <>
@@ -7,15 +8,17 @@ const Home = () => {
     <div className="container-xxl">
       <div className="row">
       <div className="banner-details p-5 d-flex flex-column align-items-start justify-content-center">
-        <span className='shadow-lg p-4 text-white'>Trade in offer !!</span>
+        <span className='shadow-lg p-4 mt-3 text-white'>Trade in offer !!</span>
         <h1>Super Value Deals</h1>
         <h2>On All products</h2>
-        <p>Save more with Apneck</p>
+        <p className='p-1 m-0 '>Save more with Apneck</p>
         <Link className='button-link mb-3'>Shop Now</Link>
       </div>
       </div>
     </div>
   </section>
+
+  <Hero />
   </>
    
   
